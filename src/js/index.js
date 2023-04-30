eef84541994a7a84063bb79ecbb76c89d8aa2222
songforthemuto <huni0423@naver.com>
2023-05-01 04:38:07 +0900

    Add Header &  Intro & Carousel(1)

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,51 @@
 (function () {
     console.log("hello world XD");
 })();
-var a = 10;
-var b = 20;
-console.log(a + b);
+// header titie logo
+var headerTitle = document.querySelector(".header__title");
+// scrolling go top
+function onClickToTop() {
+    // Issue: Modern Chromium browsers don't support smooth
+    requestAnimationFrame(function () {
+        window === null || window === void 0 ? void 0 : window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+}
+headerTitle === null || headerTitle === void 0 ? void 0 : headerTitle.addEventListener("click", onClickToTop);
+var carouselList = document.querySelector(".carousel__list");
+var carouselItems = document.querySelectorAll(".carousel__item");
+var prevButton = document.querySelector(".carousel__control--prev");
+var nextButton = document.querySelector(".carousel__control--next");
+var carouselIndex = 0;
+var timerId;
+function onClickNextCarousel() {
+    var prevItem = carouselItems[carouselIndex];
+    if (++carouselIndex >= carouselItems.length) {
+        carouselIndex = 0;
+    }
+    var nextItem = carouselItems[carouselIndex];
+    // appear next carousel
+    nextItem.classList.add("carousel__item--active");
+    // disappear prev carousel
+    prevItem.classList.remove("carousel__item--active");
+}
+function onClickPrevCarousel() {
+    var prevItem = carouselItems[carouselIndex];
+    if (--carouselIndex <= 0) {
+        carouselIndex = carouselItems.length - 1;
+    }
+    var nextItem = carouselItems[carouselIndex];
+    // appear prev carousel
+    nextItem.classList.add("carousel__item--active");
+    // disappear current carousel
+    prevItem.classList.remove("carousel__item--active");
+}
+prevButton.addEventListener("click", onClickPrevCarousel);
+nextButton.addEventListener("click", onClickNextCarousel);
+// // Function to start the automatic carousel timer
+// function startTimer() {
+//     timerId = setInterval(() => {
+//         nextCarousel();
+//     }, 5000);
+// }
+// // Start the automatic timer
+// startTimer();
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,7 +2,73 @@
     console.log("hello world XD");
 })();
 
-let a = 10;
-let b = 20;
+// header titie logo
+const headerTitle = document.querySelector(
+    ".header__title"
+) as HTMLHeadingElement;
 
-console.log(a + b);
+// scrolling go top
+function onClickToTop() {
+    // Issue: Modern Chromium browsers don't support smooth
+    requestAnimationFrame(() => {
+        window?.scrollTo({ top: 0, behavior: "smooth" });
+    });
+}
+
+headerTitle?.addEventListener("click", onClickToTop);
+
+const carouselList = document.querySelector(
+    ".carousel__list"
+) as HTMLUListElement;
+const carouselItems = document.querySelectorAll(".carousel__item");
+const prevButton = document.querySelector(
+    ".carousel__control--prev"
+) as HTMLButtonElement;
+const nextButton = document.querySelector(
+    ".carousel__control--next"
+) as HTMLButtonElement;
+let carouselIndex = 0;
+let timerId: number;
+
+function onClickNextCarousel() {
+    const prevItem = carouselItems[carouselIndex] as HTMLLIElement;
+
+    if (++carouselIndex >= carouselItems.length) {
+        carouselIndex = 0;
+    }
+
+    const nextItem = carouselItems[carouselIndex] as HTMLLIElement;
+
+    // appear next carousel
+    nextItem.classList.add("carousel__item--active");
+    // disappear prev carousel
+    prevItem.classList.remove("carousel__item--active");
+}
+
+function onClickPrevCarousel() {
+    const prevItem = carouselItems[carouselIndex] as HTMLLIElement;
+
+    if (--carouselIndex <= 0) {
+        carouselIndex = carouselItems.length - 1;
+    }
+
+    const nextItem = carouselItems[carouselIndex] as HTMLLIElement;
+
+    // appear prev carousel
+    nextItem.classList.add("carousel__item--active");
+    // disappear current carousel
+    prevItem.classList.remove("carousel__item--active");
+}
+
+prevButton.addEventListener("click", onClickPrevCarousel);
+nextButton.addEventListener("click", onClickNextCarousel);
+
+// // Function to start the automatic carousel timer
+// function startTimer() {
+//     timerId = setInterval(() => {
+//         nextCarousel();
+//     }, 5000);
+// }
+
+// // Start the automatic timer
+// startTimer();
